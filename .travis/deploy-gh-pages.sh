7eaa964252fbed765dc3785548168749cbe7d7ba
Greg F <kog@epiphanic.org>
2016-07-19 04:04:22 -0700

    Add logging to deploy-gh-pages.sh script  * Tells users what it's doing, rather than forcing them to wait for the script to finish  * Add quotation marks around origin add, in case there are funky characters in there

--- .travis/deploy-gh-pages.sh
+++ .travis/deploy-gh-pages.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+echo "beginning deployment of GitHub pages..."
+
 # We want to break if anything returns ERRNO > 0
 set -e
 
 # Create a clean working space with the latest assets
+echo "packaging assets..."
 rm -rf gh-pages && mkdir -p gh-pages
 cp -R dist/release/* gh-pages/
 
 # Init a new repo
+echo "creating new repo..."
 cd gh-pages
 git init
 git config user.name "Travis CI"
 git config user.email "root@localhost"
 
 # Re-add our origin, using our token from our previous env/global as well as a standard user/repo slug.
-git remote add origin https://${GH_PAGES_TOKEN}@github.com/kog/js-tda.git > /dev/null
+echo "adding origin..."
+git remote add origin "https://${GH_PAGES_TOKEN}@github.com/kog/js-tda.git" > /dev/null
 
 # Re-write the history of our GH-pages branch
+echo "rebuilding history..."
 git checkout -B gh-pages
 git add .
+
+echo "committing..."
 git commit -m "Update GitHub pages to latest release distribution of JS-TDA"
 
+echo "pushing..."
 git push origin gh-pages -fq > /dev/null
