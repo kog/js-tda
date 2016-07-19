e85f405dee9ead008d27c469510080da6cdbfe39
Greg F <kog@epiphanic.org>
2016-07-18 22:58:33 -0700

    Add a footer to JS-TDA displaying build information:  * Git repo, git hash, git tag (if any)  * Add repository info to package.json, looks like this was accidentally omitted  * Add gulp-util dependency to parse inboud args, which are in turn bound to Travis environment variables:    * --TRAVIS_REPO_SLUG, TRAVIS_COMMIT, TRAVIS_TAG

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const replace = require('gulp-replace');
 const tar = require('gulp-tar');
 const gzip = require('gulp-gzip');
 const bower = require('gulp-main-bower-files');
+const gutil = require('gulp-util');
 
 const pkg = require('./package.json');
 
@@ -27,6 +28,10 @@ const banner = ['/**',
     ' * Please note that this file will contain concatenated and/or minified libraries from other authors.',
     ' */',
     ''].join('\n');
+
+const git_repo = gutil.env.TRAVIS_REPO_SLUG || 'localhost/build';
+const git_hash = gutil.env.TRAVIS_COMMIT != undefined ? gutil.env.TRAVIS_COMMIT.substring(0, 8) : '[no hash]';
+const git_tag = gutil.env.TRAVIS_TAG || 'v0.0.0';
 
 gulp.task('clean', function () {
     return gulp
@@ -52,7 +57,7 @@ gulp.task('bower', ['clean'], function () {
     );
 });
 
-gulp.task('minify-js', ['bower'], function() {
+gulp.task('minify-js', ['bower'], function () {
     return gulp
         .src(['./build/libs.js', './assets/js/**/*.js'])
         .pipe(order(['libs.js', 'js']))
@@ -69,25 +74,31 @@ gulp.task('minify-css', ['bower'], function () {
     return gulp
         .src(['./assets/css/**/*', './build/libs.css'])
         .pipe(concat('js-tda.css'))
-        .pipe(replace('../fonts','fonts'))
+        .pipe(replace('../fonts', 'fonts'))
         .pipe(gulp.dest('./dist/debug'))
         .pipe(uglifyCss())
         .pipe(rename({extname: '.min.css'}))
         .pipe(gulp.dest('./dist/release'))
 });
 
-gulp.task('prepare-dists', ['minify-js', 'minify-css'], function() {
+gulp.task('prepare-dists', ['minify-js', 'minify-css'], function () {
     return eventStream.merge(
         gulp.src(['index.html', './dist/fonts', './dist/js-tda.css', './dist/js-tda.js'])
+            .pipe(replace('$GIT_REPO$', git_repo))
+            .pipe(replace('$GIT_HASH$', git_hash))
+            .pipe(replace('$GIT_TAG$', git_tag))
             .pipe(gulp.dest('./dist/debug')),
         gulp.src(['index.html', './dist/fonts', './dist/js-tda.min.css', './dist/js-tda.min.js'])
+            .pipe(replace('$GIT_REPO$', git_repo))
+            .pipe(replace('$GIT_HASH$', git_hash))
+            .pipe(replace('$GIT_TAG$', git_tag))
             .pipe(replace('js-tda.css', 'js-tda.min.css'))
             .pipe(replace('js-tda.js', 'js-tda.min.js'))
             .pipe(gulp.dest('./dist/release'))
     );
 });
 
-gulp.task('package', ['prepare-dists'], function() {
+gulp.task('package', ['prepare-dists'], function () {
     return eventStream.merge(
         gulp.src('dist/debug/**')
             .pipe(tar('debug.tar'))
